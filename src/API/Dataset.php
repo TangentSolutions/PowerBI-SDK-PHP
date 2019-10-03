8340a3a7fec141e0691a4dde450ea2a45881ad2d
Ubuntu <lhidalgosep@gmail.com>
2019-10-03 13:48:53 +0000

    fix return stament

--- src/API/Dataset.php
+++ src/API/Dataset.php
@@ -7,9 +7,7 @@ use Tngnt\PBI\Model\Dataset as DatasetModel;
 use Tngnt\PBI\Response;
 
 /**
- * Class Dataset
- *
- * @package Tngnt\PBI\Dataset
+ * Class Dataset.
  */
 class Dataset
 {
@@ -17,10 +15,9 @@ class Dataset
     const GROUP_DATASET_URL = 'https://api.powerbi.com/v1.0/myorg/groups/%s/datasets';
     const REFRESH_DATASET_URL = 'https://api.powerbi.com/v1.0/myorg/datasets/%s/refreshes';
     const GROUP_REFRESH_DATASET_URL = 'https://api.powerbi.com/v1.0/myorg/groups/%s/datasets/%s/refreshes';
-    
 
     /**
-     * The SDK client
+     * The SDK client.
      *
      * @var Client
      */
@@ -37,9 +34,9 @@ class Dataset
     }
 
     /**
-     * Retrieves the datasets from the PowerBI API
+     * Retrieves the datasets from the PowerBI API.
      *
-     * @param null|string $groupId An optional group ID
+     * @param string|null $groupId An optional group ID
      *
      * @return Response
      */
@@ -51,33 +48,33 @@ class Dataset
 
         return $this->client->generateResponse($response);
     }
-    
-      /**
-     * Refresh the dataset from the PowerBI API
+
+    /**
+     * Refresh the dataset from the PowerBI API.
      *
-     * @param string $datasetId An dataset ID
-     *
-     * @param null|string $groupId An optional group ID
-     *
-     * @param null|bool $notify set if user recibe notify mail
+     * @param string      $datasetId An dataset ID
+     * @param string|null $groupId   An optional group ID
+     * @param bool|null   $notify    set if user recibe notify mail
      *
      * @return Response
      */
-    public function getDatasets($datasetId, $groupId = null,$notify = false)
+    public function getDatasets($datasetId, $groupId = null, $notify = false)
     {
-        $url = $this->getRefreshUrl($groupId,$datasetId);
-        if($notify)
-            $response = $this->client->request(Client::METHOD_POST, $url,["notifyOption" => "MailOnFailure"]);
-        else
+        $url = $this->getRefreshUrl($groupId, $datasetId);
+        if ($notify) {
+            $response = $this->client->request(Client::METHOD_POST, $url, ['notifyOption' => 'MailOnFailure']);
+        } else {
             $response = $this->client->request(Client::METHOD_POST, $url);
+        }
+
         return $this->client->generateResponse($response);
     }
 
     /**
-     * Create a new dataset on PowerBI
+     * Create a new dataset on PowerBI.
      *
      * @param DatasetModel $dataset The dataset model
-     * @param null|string  $groupId An optional group ID
+     * @param string|null  $groupId An optional group ID
      *
      * @return Response
      */
@@ -91,9 +88,9 @@ class Dataset
     }
 
     /**
-     * Helper function to format the request URL
+     * Helper function to format the request URL.
      *
-     * @param null|string $groupId An optional group ID
+     * @param string|null $groupId An optional group ID
      *
      * @return string
      */
@@ -105,21 +102,21 @@ class Dataset
 
         return self::DATASET_URL;
     }
-    
-     /**
-     * Helper function to format the request URL
+
+    /**
+     * Helper function to format the request URL.
      *
-     * @param string $datasetId id from dataset
-     *
-     * @param null|string $groupId An optional group ID
+     * @param string      $datasetId id from dataset
+     * @param string|null $groupId   An optional group ID
      *
      * @return string
      */
     private function getRefreshUrl($datasetId, $groupId)
     {
         if ($groupId) {
-            return sprintf(self::GROUP_REFRESH_DATASET_URL, $datasetId,$reportId);
+            return sprintf(self::GROUP_REFRESH_DATASET_URL, $datasetId, $reportId);
         }
-        return return sprintf(self::REFRESH_DATASET_URL,$datasetId);
+
+        return sprintf(self::REFRESH_DATASET_URL, $datasetId);
     }
 }
